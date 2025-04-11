957b251f91da0d66272e944389265d3742a50120
Oksana Melnyk <69401769+comoartista@users.noreply.github.com>
2025-04-11 11:26:49 +0200

    update API URL for production

---
+++ client/src/js/config.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = import.meta.env.PROD
+  ? "https://ecommerce-js-2rb5.onrender.com"
+  : "http://localhost:5001";
--- client/src/js/modules/products.js
+++ client/src/js/modules/products.js
@@ -1,29 +1,24 @@
+import { API_BASE_URL } from "../config";
+
 let productsList = [];
 
 // Fetches products from the API and caches the result
 export async function fetchProducts() {
-  // Check if the products list is empty before fetching data
   if (productsList.length === 0) {
     try {
-      // Fetch product data from the API
-      const response = await fetch("http://localhost:5001/api/products");
-      
-      // If the response is not ok, throw an error
+      const response = await fetch(`${API_BASE_URL}/api/products`);
+
       if (!response.ok) {
         throw new Error("Error loading data");
       }
-      
-      // Parse the JSON data from the response
+
       const result = await response.json();
-      
-      // Store the fetched products in the productsList array
+
       productsList = result.data;
     } catch (error) {
-      // Log any errors encountered during the fetch process
       console.error(error.message);
     }
   }
-  
-  // Return the products list (cached data after the first fetch)
+
   return productsList;
 }
